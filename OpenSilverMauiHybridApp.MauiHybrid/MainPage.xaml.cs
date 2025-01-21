59c5a5f57838a72c7b58affd0b9f4a4df378c6ef
Iakov Lilo <lilo-jasha@yandex.ru>
2025-01-21 15:25:37 +1100

    New project template

---
+++ OpenSilverMauiHybridApp.MauiHybrid/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenSilverMauiHybridApp.MauiHybrid;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ OpenSilverMauiHybridApp.MauiHybrid/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace OpenSilverMauiHybridApp.MauiHybrid;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
